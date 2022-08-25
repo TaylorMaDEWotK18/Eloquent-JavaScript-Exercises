f4626cb14091f0e91a85c740e2638839f844053a
TaylorMade <warriorotk18@gmail.com>
2022-08-25 00:11:06 -0600

    tried to use some banned names for variables and both worked, must have changed

--- chp-2.js
+++ chp-2.js
@@ -8,3 +8,7 @@ console.log(luigiDebt);
 
 let luigi;
 console.log(luigi);
+
+//It said 'package' and 'private' were keywords that could not be used as variable/binding names but tested both and it must have changed
+let private = 3;
+console.log(private);
