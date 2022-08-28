4064ee31924558e1f57e0e83b8779d42209d0507
TaylorMade <warriorotk18@gmail.com>
2022-08-28 02:10:33 -0600

    a for loop exercise to get 2 to the 10th power

--- chp-2.js
+++ chp-2.js
@@ -12,3 +12,10 @@ console.log(luigi);
 //It said 'package' and 'private' were keywords that could not be used as variable/binding names but tested both and it must have changed
 let private = 3;
 console.log(private);
+
+//Learning about for loops and this loop computes 2 to the 10th power
+let result = 1;
+for (let counter = 0; counter < 10; counter = counter + 1) {
+	result = result * 2;
+}
+console.log(result);
